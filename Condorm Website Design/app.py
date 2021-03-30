a0b4a103099d7486d3d9c3493a41aac4678397ab
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2021-03-29 21:35:45 -0500

    Added Files

--- Condorm Website Design/app.py
+++ Condorm Website Design/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+from forms import *
+
 app = Flask(__name__)
+app.secret_key = 'meme'
 
-ENV = 'prod'
-
+ENV = 'dev'
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Brad3nlive01@localhost/condorm'
@@ -13,11 +15,40 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fogscmxflbvfpn:cdc2900b405304e95b4cae360506a382899386eb3f54c4c2fc24c65734c49622@ec2-54-242-43-231.compute-1.amazonaws.com:5432/d2j0ha8pcp3qr8'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    firstname = db.Column(db.String(), nullable = False)
+    lastname = db.Column(db.String(), nullable = False)
+    email = db.Column(db.String(50), unique = True, nullable = False)
+    password = db.Column(db.String(20), nullable = False)
+    dormname = db.Column(db.String(), nullable = False)
+    roomnum = db.Column(db.Integer(), nullable = False)
+    admin = db.Column(db.Boolean(), nullable = False)
+
+    def __init__(self, username, firstname, lastname, email, password, dormname, roomnum, admin):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+        self.dormname = dormname
+        self.roomnum = roomnum
+        self.admin = admin
 
 class Orders(db.Model):
     __tablename__ = "orders"
+    id = db.Column(db.Integer, primary_key = True)
+
+    def __init__(self, product, quantity):
+        self.product = product
+        self.quantity = quantity
+
+class Products(db.Model):
+    __tablename__ = "products"
     id = db.Column(db.Integer, primary_key = True)
     product = db.Column(db.String(200))
     quantity = db.Column(db.Integer)
@@ -26,20 +57,19 @@ class Orders(db.Model):
         self.product = product
         self.quantity = quantity
 
-@app.route('/')
-def mainpage():
-    return render_template('login.html')
 
-@app.route('/main', methods= ['POST', 'GET'])
+
+@app.route('/', methods = ['POST', 'GET'])
+def mainpage():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('login.html', form = login_form)
+
+
+@app.route('/main')
 def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'bldresse' and password == 'password123':
-            return render_template('main.html')
-    if request.method == 'GET':
-        return render_template('main.html')
-    return render_template('login.html', message = 'Invalid username or password')
+    return render_template('main.html')
     
  
 @app.route('/about')
@@ -48,7 +78,7 @@ def about():
 
 @app.route('/order')
 def order():
-    return render_template('order.html')
+    return render_template('order.html', products = Products.query.all())
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -71,9 +101,33 @@ def resources():
 def information():
     return render_template('update.html')
 
-@app.route('/registration')
+@app.route('/registration', methods = ['GET', 'POST'])
 def registration():
-    return render_template('registration.html')
+
+    reg_form = RegistrationForm()
+
+    if reg_form.validate_on_submit():
+        username = reg_form.username.data
+        firstname = reg_form.firstname.data
+        lastname = reg_form.lastname.data
+        email = reg_form.email.data
+        password = reg_form.password.data
+        dormname = reg_form.dormname.data
+        roomnum = reg_form.roomnum.data
+        admin = False
+
+        user_object = User.query.filter_by(username= username).first()
+        email_object = User.query.filter_by(email = email).first()
+
+        if user_object:
+            return render_template('registration.html', form = reg_form, message = "Someone has taken that username!")
+        if email_object:
+            return render_template('registration.html', form = reg_form, message = "Someone has taken that email!")
+        user = User(username = username,firstname = firstname, lastname = lastname, email = email, password = password, dormname = dormname, roomnum = roomnum, admin = admin)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('mainpage'))
+    return render_template('registration.html', form = reg_form)
 
 @app.route('/created', methods = ['POST'])
 def created():
@@ -92,3 +146,4 @@ def created():
     
 if __name__ == "__main__":
     app.run()
+    
---
+++ Condorm Website Design/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, IntegerField, SelectField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, EqualTo, ValidationError
+#from app import User
+
+def checker(form, field):
+    username_entered = form.username.data
+    password_entered = field.data
+
+    user_object = User.query.filter_by(username = username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or password is incorrect")
+    elif password_entered != user_object.password:
+        raise ValidationError("Username or password is incorrect")
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('username', 
+                validators=[InputRequired(message = "Username required"),
+                Length(min = 4, max = 20, message= "Username must be between 4 and 20 characters")])
+    firstname = StringField('firstname', 
+                validators=[InputRequired(message = "First Name required")])
+    lastname = StringField('lastname', 
+                validators=[InputRequired(message = "Last Name required")])
+    email = StringField('email',
+                validators=[InputRequired(message= "Email required"),
+                Email(message = 'Invalid Email'),
+                Length(max= 50)])
+    password = PasswordField('password', 
+                validators=[InputRequired(message = "Password required"),
+                Length(min = 4, max = 20, message= "Password must be between 4 and 20 characters")])
+    repass = PasswordField('repass', 
+                validators=[InputRequired(message = "Password required"),
+                EqualTo('password', message = "Passwords must match")])
+    dormname = SelectField('dormname',
+                choices = ['Founders', 'Futrall', 'Gibson','Gregson','Holcombe','Hotz','Humphreys','Pomfret','Reid','Yocum'])
+    roomnum = IntegerField('roomnum',
+                validators=[InputRequired(message = "Room Number required")])
+    submitbtn = SubmitField('Create Account')
+
+class LoginForm(FlaskForm):
+    username = StringField('username', 
+                validators=[InputRequired(message = "Username required")])
+    password = PasswordField('password', 
+                validators=[InputRequired(message = "Password required"),
+                checker])
+    submitbtn = SubmitField('Login')
