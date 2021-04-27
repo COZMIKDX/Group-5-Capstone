364495fb1c9001f0b4c73b20b78896a40d2ccb22
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2021-04-27 00:32:55 -0500

    Going to Heroku

--- Condorm Website Design/app.py
+++ Condorm Website Design/app.py
@@ -4,19 +4,25 @@ from flask_login import LoginManager, UserMixin, login_user, current_user, login
 from flask_mail import Mail, Message
 from passlib.hash import pbkdf2_sha256
 
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 from forms import *
 
 app = Flask(__name__)
 #something crazy later
 app.secret_key = 'meme'
 
-ENV = 'prod'
+ENV = 'dev'
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Brad3nlive01@localhost/condorm'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zafiblysdeutxn:8e4232b39ad16ce2bde93eeb79cd2036e01943b33fde788521f4358ee0520145@ec2-54-164-22-242.compute-1.amazonaws.com:5432/d7dpc8egbb7s3t'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kbqbgcqghvqwul:11d356f3b6f22a190cc58e471855f90f73bc107ae29a120e4dc245c224d8a024@ec2-52-87-107-83.compute-1.amazonaws.com:5432/davsklgj1tk9eo'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -33,15 +39,16 @@ login.init_app(app)
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, unique = True, primary_key = True)
-    username = db.Column(db.String(20), unique = True, nullable = False)
-    firstname = db.Column(db.String(), nullable = False)
-    lastname = db.Column(db.String(), nullable = False)
-    email = db.Column(db.String(50), unique = True, nullable = False)
-    password = db.Column(db.String(), nullable = False)
-    dormname = db.Column(db.String(), nullable = False)
-    roomnum = db.Column(db.Integer(), nullable = False)
-    admin = db.Column(db.Boolean(), nullable = False)
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    firstname = db.Column(db.String(), nullable=False)
+    lastname = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    dormname = db.Column(db.String(), nullable=False)
+    roomnum = db.Column(db.Integer(), nullable=False)
+    admin = db.Column(db.Boolean(), nullable=False)
+    children = relationship("Orders")
 
     def __init__(self, username, firstname, lastname, email, password, dormname, roomnum, admin):
         self.username = username
@@ -55,24 +62,33 @@ class User(UserMixin, db.Model):
 
 class Orders(db.Model):
     __tablename__ = "orders"
-    id = db.Column(db.Integer, unique = True, primary_key = True)
-    #Person id
-    #Product id(s)
-    #Status
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    user_id = Column(db.Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="children")
+    product_id = Column(db.Integer, ForeignKey('products.id'))
+    quantity = db.Column(db.Integer, nullable = False)
+    # This only includes the ID for a single product. We will implement expansions to that later.
+    user = relationship("Products", back_populates="children")
+    status = db.Column(db.Boolean(), nullable=False)
 
-    def __init__(self, product, quantity):
-        self.product = product
+    def __init__(self, user_id, product_id, quantity, status):
+        self.user_id = user_id
+        self.product_id = product_id
         self.quantity = quantity
+        self.status = status
 
 class Products(db.Model):
     __tablename__ = "products"
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     product = db.Column(db.String(200))
     quantity = db.Column(db.Integer)
+    image = db.Column(db.String(100))
+    children = relationship("Orders")
 
-    def __init__(self, product, quantity):
+    def __init__(self, product, quantity, image):
         self.product = product
         self.quantity = quantity
+        self.image = image
 
 
 @login.user_loader
@@ -105,20 +121,30 @@ def about():
 @app.route('/order')
 def order():
     if not current_user.is_authenticated:
-        return "Please login to place an order"
-    return render_template('order.html')
+        return "Please login to place an order" #Error Message#
+    products = Products.query.all()
+    return render_template('order.html', products = products)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
+        user_id = current_user.id
         product = request.form['products']
-        quantity = request.form['quantity']
+        quantity = int(request.form['quantity'])
+        status = False
+
         if product == '' or quantity == '':
             return render_template('order.html', message = 'Please enter a valid product or quantity')
 
-        data = Orders(product, quantity)
-        db.session.add(data)
-        db.session.commit()
+        prod_object = Products.query.filter_by(id = product).first()
+        if prod_object.quantity >= quantity:
+            prod_object.quantity = prod_object.quantity - quantity
+            data = Orders(user_id, product, quantity, status)
+            db.session.add(data)
+            db.session.commit()
+        else:
+            return "not enough"
+            #Error Message not enough quantity#
         return render_template('submit.html')
     
 @app.route('/resources')
@@ -144,7 +170,7 @@ def registration():
     reg_form = RegistrationForm()
 
     if reg_form.validate_on_submit():
-        username = reg_form.username.data
+        username = str(reg_form.username.data)
         firstname = reg_form.firstname.data
         lastname = reg_form.lastname.data
         email = reg_form.email.data
@@ -153,6 +179,8 @@ def registration():
         roomnum = reg_form.roomnum.data
         admin = False
 
+        if username == "admin":
+            admin = True
         password_hashed = pbkdf2_sha256.hash(password)
 
         user_object = User.query.filter_by(username= username).first()
@@ -171,20 +199,64 @@ def registration():
         return redirect(url_for('mainpage'))
     return render_template('registration.html', form = reg_form)
 
-@app.route('/created', methods = ['POST'])
-def created():
+@app.route("/admin", methods = ['GET', 'POST'])
+def admin():
+    if current_user.admin == False:
+        return str(current_user.admin) #ERROR MESSAGE + ADD LOGIN REQUIREMENT#
+    return render_template('admin.html')
+
+@app.route("/orderlist", methods = ['GET', 'POST'])
+def orderlist():
+    #if current_user.admin == False:
+        #return str(current_user.admin) #ERROR MESSAGE + ADD LOGIN REQUIREMENT#
+    if request.method == "POST":
+        orderid = request.form['ordern']
+        order_object = Orders.query.filter_by(id = orderid).first()
+        order_object.status = True
+        db.session.commit()
+
+    orders = Orders.query.all()
+    return render_template('orderlist.html', orders = orders)
+
+@app.route("/products", methods = ['GET', 'POST'])
+def products():
+    #if current_user.admin == False:
+        #return str(current_user.admin) #ERROR MESSAGE + ADD LOGIN REQUIREMENT#
     if request.method == 'POST':
-        user = request.form['username']
-        passw = request.form['password']
-        repass = request.form['repass']
-        dormname = request.form['dormname']
-        roomnum = request.form['roomnum']
-        if user == '' or passw == '' or dormname == '' or roomnum == '':
-            return render_template('registration.html', message = 'Missing required information!')
-        if repass != passw:
-            return render_template('registration.html', message = "Passwords do not match up. Try Again!")
-        else:
-            return render_template('created.html')
+        productid = request.form['productn']
+        product_object = Products.query.filter_by(id = productid).first()
+        db.session.delete(product_object)
+        db.session.commit()
+
+    products = Products.query.all()
+    return render_template('products.html', products = products)
+
+@app.route("/users", methods = ['GET', 'POST'])
+def users():
+    #if current_user.admin == False:
+        #return str(current_user.admin) #ERROR MESSAGE + ADD LOGIN REQUIREMENT#
+    if request.method == 'POST':
+        userid = request.form['usern']
+        user_object = User.query.filter_by(id = userid).first()
+        user_object.admin = True
+        db.session.commit()
+
+    users = User.query.all()
+    return render_template('users.html', users = users)
+
+@app.route("/new_product", methods = ['GET', 'POST'])
+def new_product():
+    new_productform = ProductForm()
+    if new_productform.validate_on_submit():
+        product = new_productform.productname.data
+        quantity = new_productform.quantity.data
+        image = new_productform.image.data
+
+        item = Products(product, quantity, image)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('products'))
+    return render_template('new_product.html', form = new_productform)
 
 @app.route("/logout", methods = ["GET"])
 def logout():
--- Condorm Website Design/forms.py
+++ Condorm Website Design/forms.py
@@ -6,18 +6,18 @@ from passlib.hash import pbkdf2_sha256
 class RegistrationForm(FlaskForm):
     username = StringField('username', 
                 validators=[InputRequired(message = "Username required"),
-                Length(min = 4, max = 20, message= "Username must be between 4 and 20 characters")])
+                Length(min = 4, max = 20, message = "Username must be between 4 and 20 characters")])
     firstname = StringField('firstname', 
                 validators=[InputRequired(message = "First Name required")])
     lastname = StringField('lastname', 
                 validators=[InputRequired(message = "Last Name required")])
     email = StringField('email',
-                validators=[InputRequired(message= "Email required"),
+                validators=[InputRequired(message = "Email required"),
                 Email(message = 'Invalid Email'),
                 Length(max= 50)])
     password = PasswordField('password', 
                 validators=[InputRequired(message = "Password required"),
-                Length(min = 4, max = 20, message= "Password must be between 4 and 20 characters")])
+                Length(min = 4, max = 20, message = "Password must be between 4 and 20 characters")])
     repass = PasswordField('repass', 
                 validators=[InputRequired(message = "Password required"),
                 EqualTo('password', message = "Passwords must match")])
@@ -36,7 +36,16 @@ class LoginForm(FlaskForm):
 
 class UpdateForm(FlaskForm):
     dormname = SelectField('dormname',
-                choices = ['Founders', 'Futrall', 'Gibson','Gregson','Holcombe','Hotz','Humphreys','Pomfret','Reid','Yocum'])
+                choices = ['Adohi', 'Clark', 'Duncan', 'Founders', 'Futrall', 'Gatewood', 'Gibson', 'Gregson', 'Harding', 'Holcombe', 'Hotz', 'Humphreys', 'Maple Hill East', 'Maple Hill South', 'Maple Hill West', 'Morgan', 'Walton', 'Pomfret', 'Reid', 'Yocum'])
     roomnum = IntegerField('roomnum',
                 validators=[InputRequired(message = "Room Number required")])
     submitbtn = SubmitField('Update Information')
+
+class ProductForm(FlaskForm):
+    productname = StringField('productname',
+                validators=[InputRequired(message = "Product name required")])
+    quantity = IntegerField('quantity',
+                validators=[InputRequired(message= "Quantity amount required")])
+    image =     StringField('image',
+                validators=[InputRequired(message = "Image required")])
+    submitbtn = SubmitField('Add Product')
